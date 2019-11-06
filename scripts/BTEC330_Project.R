1ec70c95403a6a2c5e31fc670402f4616c5fbd5c
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2019-11-06 11:53:13 -0500

    Update BTEC330_Project.R

--- scripts/BTEC330_Project.R
+++ scripts/BTEC330_Project.R
@@ -26,6 +26,17 @@ sink('data_output/LDH_regression.txt', append = TRUE)
 print(LDH.regression)
 sink()
 
+## ANOVA: IBS-subtype vs. Bloodwork parameter
+## http://www.sthda.com/english/wiki/one-way-anova-test-in-r
+LDH.aov <- aov(LDH ~ IBS.subtype, data = IBS)
+summary(LDH.aov)
+sink('data_output/LDH_anova.txt', append = TRUE)
+print(LDH.aov)
+sink()
+
+## Print scatterplot and box plots as .png files into "fig_output" project directory.
+## http://www.sthda.com/english/wiki/ggsave-save-a-ggplot-r-software-and-data-visualization
+
 ## Scatterplots
 ## https://www.statmethods.net/graphs/scatterplot.html
 
@@ -33,22 +44,23 @@ ggplot(IBS, aes(x = BMI, y = LDH)) +
   geom_point() +    
   geom_smooth(method = lm) 
 
-## ANOVA: IBS-subtype vs. Bloodwork parameter
-## http://www.sthda.com/english/wiki/one-way-anova-test-in-r
-LDH.aov <- aov(LDH ~ IBS.subtype, data = IBS)
-summary(LDH.aov)
+png("fig_output/LDH_scatterplot.png")
+LDH_scatterplot <- ggplot(IBS, aes(x = BMI, y = LDH)) +
+  geom_point() +    
+  geom_smooth(method = lm) 
+       )
+print(LDH_scatterplot)
+dev.off()
 
 ## Box plots
 ## https://www.statmethods.net/graphs/boxplot.html
-boxplot(LDH ~ IBS.subtype, data = IBS, main="LDH by IBS subtype", 
-       xlab = "IBS.subtype", ylab = "LDH"
-       )
 
-## Print .png files of images into "Fig_output" projects directory.
-## http://www.sthda.com/english/wiki/ggsave-save-a-ggplot-r-software-and-data-visualization
-png("LDH_boxplot.png")
+png("fig_output/LDH_boxplot.png")
 LDH_boxplot <- boxplot(LDH ~ IBS.subtype, data = IBS, main="LDH by IBS subtype", 
        xlab = "IBS.subtype", ylab = "LDH"
        )
-print(fig_output/LDH_boxplot)
+print(LDH_boxplot)
 dev.off()
+
+
+
