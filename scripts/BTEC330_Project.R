a392a6b161ccc26c446afbb82f3abfcd0547dd82
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2019-11-06 11:00:43 -0500

    Update and rename BTEC330_Template.R to BTEC330_Project.R

---
+++ scripts/BTEC330_Project.R
@@ -0,0 +1,37 @@
+
+## BTEC330 Robinson Project2
+
+## Install necessary packages
+install.packages("ggplot2")
+library(ggplot2)
+
+## Read data
+IBS <- read.csv("data/RobinsonEtAl_Sup1.csv", header = TRUE)
+head(IBS)
+write.csv(IBS, "data_output/LDH.csv")
+
+##  Single Regressions 
+##  Data obtained from Robinson, et al. 2019 (doi: https://doi.org/10.1101/608208)
+##  https://statquest.org/2017/10/30/statquest-multiple-regression-in-r/
+##  http://www.sthda.com/english/articles/40-regression-analysis/167-simple-linear-regression-in-r/
+##  http://r-statistics.co/Linear-Regression.html
+
+## Single Regression Test, BMI vs. Bloodwork parameter
+single.regression <- lm(BMI ~ LDH, data=IBS1)
+summary(single.regression)
+
+## Scatterplots
+## https://www.statmethods.net/graphs/scatterplot.html
+
+ggplot(IBS1, aes(x=BMI, y=LDH)) +
+  geom_point() +    
+  geom_smooth(method=lm) 
+
+## ANOVA Tests IBS-subtypes vs. Bloodwork parameter
+## http://www.sthda.com/english/wiki/one-way-anova-test-in-r
+LDH.aov <- aov(LDH ~ IBS.subtype, data = IBS)
+summary(res.aov)
+
+## Print .png files of images into "Fig_output" projects directory.
+##http://www.sthda.com/english/wiki/ggsave-save-a-ggplot-r-software-and-data-visualization
+
